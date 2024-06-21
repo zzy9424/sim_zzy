4e61aec1a9e0a5f2402cb625fe1f704352db2db7
dr <1102310490@qq.com>
2024-06-21 08:19:47 +0800

    1

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ import yaml
 file_path="output.yaml"
 with open(file_path, 'r') as file:
     config = yaml.safe_load(file)
-
+config = {
+    'lidar_conf.max_dist': 3,
+    'lidar_conf.num_bins': 16,
+    'placements_conf.extents': [-1.5, -1.5, 1.5, 1.5],
+    'Hazards': {
+        'num': 1,
+        'size': 0.7,
+        'locations': [(0, 0)],
+        'is_lidar_observed': True,
+        'is_constrained': True,
+        'keepout': 0.705,
+    },
+    'Goal': {
+        'keepout': 0.305,
+        'size': 0.3,
+        'locations': [(1.1, 1.1)],
+        'is_lidar_observed': True,
+    },
+}
 env_id = 'SafetyPointGoalBase-v0'
 env = safety_gymnasium.make(env_id, render_mode="human",config=config)
 # env = safety_gymnasium.make(env_id)
